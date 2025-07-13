46dec61ecf9841b219dfcefc6e80672d5a7fd738
mzakharow <mzakharov85@gmail.com>
2025-07-12 23:32:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from datetime import datetime
+from datetime import datetime, timedelta
 from content import survey, teamlead_view, settings, reports
 from auth import get_user_by_email, register_user, is_team_lead, is_admin, hash_password, check_password, generate_token
 
