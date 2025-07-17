113e09ec6cc3569a7d9125fdb85cef3b4192f39d
mzakharow <mzakharov85@gmail.com>
2025-07-17 00:02:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,15 @@ if not st.user.is_logged_in:
     
     if st.button("Authenticate"):
         st.login("google")
+
+        
         # user = st.session_state.user
-        user = get_user_by_email(st.user.email)
-        if user:
-            st.session_state.user = user
-            st.success(user["name"])
-            st.session_state.email_checked = True
-            user = st.session_state.user
+        # user = get_user_by_email(st.user.email)
+        # if user:
+        #     st.session_state.user = user
+        #     st.success(user["name"])
+        #     st.session_state.email_checked = True
+        #     user = st.session_state.user
 else:
     # if user:
     #     st.session_state.user = user
@@ -36,6 +38,12 @@ else:
         st.session_state.user = None
         # st.rerun()
 
+user = get_user_by_email(st.user.email)
+if user:
+    # st.session_state.user = user
+    st.success(user["name"])
+    st.session_state.email_checked = True
+    # user = st.session_state.user
 
 # user = get_user_by_email(st.user.email)
 # if user:
