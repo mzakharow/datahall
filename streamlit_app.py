1915efc275913a62fdf7d0e28a27606d91f112d8
mzakharow <mzakharov85@gmail.com>
2025-07-16 23:44:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,11 +28,11 @@ else:
         # st.rerun()
 
 
-    user = get_user_by_email(st.user.email)
-    if user:
-        st.session_state.user = user
-        st.success(st.user.email)
-        st.session_state.email_checked = True
+user = get_user_by_email(st.user.email)
+if user:
+    st.session_state.user = user
+    st.success(st.user.email)
+    st.session_state.email_checked = True
 
     # col_space, col_buttons = st.columns([10, 2]) 
     # with col_buttons:
