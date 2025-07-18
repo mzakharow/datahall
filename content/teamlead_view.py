17ae4f245289b6a4aad7ac8ee878b871ddd23f8a
mzakharow <mzakharov85@gmail.com>
2025-07-17 22:18:52 -0500

    Update teamlead_view.py

--- content/teamlead_view.py
+++ content/teamlead_view.py
@@ -78,7 +78,7 @@ def run():
         "Activity": next((act.name for act in activities if act.id == latest_tasks.get(tech.id, {}).get("activity_id")), list(act_options.keys())[0]),
         "Cable Type": cable_id_to_name.get(latest_tasks.get(tech.id, {}).get("cable_type_id"), list(cable_options.keys())[0]),
         "Rack": latest_tasks.get(tech.id, {}).get("rack", ""),
-        "Team Lead": team_lead_name,
+        "Team lead": team_lead_name,
         # "Quantity": latest_tasks.get(tech.id, {}).get("quantity", 0),
         # "Percent": latest_tasks.get(tech.id, {}).get("percent", 0),
         "Time": latest_tasks.get(tech.id, {}).get("timestamp", "").astimezone(ZoneInfo(LOCAL_TIMEZONE)).strftime("%H:%M") if latest_tasks.get(tech.id, {}).get("timestamp") else ""
@@ -97,7 +97,8 @@ def run():
                 "Location": st.column_config.SelectboxColumn("Location", options=list(loc_options.keys())),
                 "Activity": st.column_config.SelectboxColumn("Activity", options=list(act_options.keys())),
                 "Cable Type": st.column_config.SelectboxColumn("Cable Type", options=list(cable_options.keys())),
-                "Rack": st.column_config.TextColumn("Rack", max_chars=5)
+                "Rack": st.column_config.TextColumn("Rack", max_chars=5),
+                "Team lead": st.column_config.SelectboxColumn("Team lead", options=list(team_lead_name.keys())),
                 # "Quantity": st.column_config.NumberColumn("Quantity", min_value=0, step=1, default=0),
                 # "Percent": st.column_config.NumberColumn("Percent", min_value=0, max_value=100, step=1, default=0)
             }
